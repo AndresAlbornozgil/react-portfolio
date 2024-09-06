70938d7ffaceb8c8e54579aa094d7a06ef97e549
AA <aalborgil002@gmail.com>
2024-09-06 18:45:03 -0400

    Worked on styling

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -8,7 +8,7 @@ function AboutMe() {
       <div id='aboutme-container' className="container col-xxl-8 px-4 py-5">
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div className="col-lg-6">
-        <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Hello, I'm Andres</h1>
+        <h1 id='aboutMeTitle' className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Hello, I'm Andres</h1>
         <strong><p id='aboutMeParagraph' className="lead">A Full-Stack Web Developer passionate about crafting meaningful experiences. Whether it's designing intuitive user interfaces or building robust back-end systems, I'm all about bringing ideas to life on the web.</p></strong>
       </div>
       <div className="col-10 col-sm-8 col-lg-6">
